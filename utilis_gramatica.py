4158698c1d0718caa72126ca2197290d3989a1d4
Luis Esteban <luis_esteban_95@hotmail.es>
2020-04-16 23:42:35 +0200

    Documentación 1

--- utilis_gramatica.py
+++ utilis_gramatica.py
@@ -5,6 +5,12 @@ import pandas as pd
 
 
 def op_0(a, b):
+    """
+    Operación 0 de la gramática. Suma
+    :param a: primera expresión
+    :param b: Segunda expresión
+    :return:
+    """
     try:
         return a + b
     except Exception:
@@ -12,6 +18,12 @@ def op_0(a, b):
 
 
 def op_1(a, b):
+    """
+    Operación 1 de la gramática. Resta
+    :param a: primera expresión
+    :param b: Segunda expresión
+    :return:
+    """
     try:
         return a - b
     except Exception:
@@ -19,6 +31,12 @@ def op_1(a, b):
 
 
 def op_2(a, b):
+    """
+    Operación 2 de la gramática. Multiplicación
+    :param a: primera expresión
+    :param b: Segunda expresión
+    :return:
+    """
     try:
         return a * b
     except Exception:
@@ -26,6 +44,12 @@ def op_2(a, b):
 
 
 def op_3(a, b):
+    """
+    Operación 3 de la gramática. División
+    :param a: primera expresión
+    :param b: Segunda expresión
+    :return:
+    """
     try:
         return a / b
     except Exception:
@@ -33,6 +57,11 @@ def op_3(a, b):
 
 
 def pre_op_0(x):
+    """
+    Pre operación 0 de la gramática. Seno
+    :param x: Expresión
+    :return:
+    """
     try:
         return np.sin(x)
     except:
@@ -40,6 +69,11 @@ def pre_op_0(x):
 
 
 def pre_op_1(x):
+    """
+    Pre operación 1 de la gramática. Coseno
+    :param x: Expresión
+    :return:
+    """
     try:
         return np.cos(x)
     except:
@@ -47,6 +81,11 @@ def pre_op_1(x):
 
 
 def pre_op_2(x):
+    """
+    Pre operación 0 de la gramática. Exponencial
+    :param x: Expresión
+    :return:
+    """
     try:
         return np.exp(x)
     except:
@@ -54,6 +93,11 @@ def pre_op_2(x):
 
 
 def pre_op_3(x):
+    """
+    Pre operación 3 de la gramática. Logaritmo
+    :param x: Expresión
+    :return:
+    """
     if x > 0:
         try:
             return np.log(x)
@@ -64,6 +108,13 @@ def pre_op_3(x):
 
 
 def expr_0(exp1, op, exp2):
+    """
+    Expresión 0 de la gramática
+    :param exp1: Expresión 1
+    :param op: Operación
+    :param exp2: Expresión 2
+    :return:
+    """
     try:
         return op(exp1, exp2)
     except:
@@ -71,6 +122,13 @@ def expr_0(exp1, op, exp2):
 
 
 def expr_1(exp1, op, exp2):
+    """
+    Expresión 1 de la gramática
+    :param exp1: Expresión 1
+    :param op: Operación
+    :param exp2: Expresión 2
+    :return:
+    """
     try:
         return (op(exp1, exp2))
     except:
@@ -78,6 +136,12 @@ def expr_1(exp1, op, exp2):
 
 
 def expr_2(pre, exp1):
+    """
+    Expresión 2 de la gramática
+    :param exp1: Expresión 1
+    :param op: Operación
+    :return:
+    """
     try:
         return pre(exp1)
     except:
@@ -85,12 +149,17 @@ def expr_2(pre, exp1):
 
 
 def expr_3(var):
+    """
+    Expresión 3 de la gramática
+    :param var: Variable
+    :return:
+    """
     try:
         return var
     except:
         raise ValueError(' Cadena no compatible ')
 
-
+# Conjuntos de las oepraciones de la gramática
 expr = ([expr_0, 3], [expr_1, 3], [expr_2, 2], [expr_3, 1])
 dic_pre_op = (pre_op_0, pre_op_1, pre_op_2, pre_op_3)
 dict_var = (lambda x: x, lambda x: x, 1, 1)
@@ -98,6 +167,13 @@ dict_op = (op_0, op_1, op_2, op_3)
 
 
 def ret_op(codon, x, cod_index=0):
+    """
+    Función que implementa la decodificación de cadena de números a función matemática
+    :param codon: Cadena de números del 0 al 3
+    :param x: Valor del puntoa a evaluar
+    :param cod_index: Código del índice que se quiere tomar para empezar a decodificar la cadena
+    :return:
+    """
     if cod_index < len(codon):
         cod_tmp = codon[cod_index]
         if cod_tmp == '0':
@@ -138,14 +214,39 @@ def ret_op(codon, x, cod_index=0):
 
 
 def genera_codon(min=0, max=4, longitud=60):
+    """
+    Función que devuelve un genotipo
+    :param min: Valor mínimo de los valores del fenotipo
+    :param max: Valor máximo de los valores del fenotipo
+    :param longitud: Longitud del fenotipo
+    :return:
+    """
     return ''.join([str(np.random.randint(min, max)) for x in range(longitud)])
 
 
 def df(f, A, dx, h, i):
+    """
+    Devuelve la derivada numérica de una función en un punto
+    :param f: función
+    :param A: Límite inferior del intervalo
+    :param dx: variación para el cálculo de la derivada
+    :param h: Parámetro diferencial
+    :param i: Posición del intervalo sobre el que calcular la derivada
+    :return:
+    """
     return (f((A + i * dx) + h) - f(A + i * dx)) / h
 
 
 def error(gen_values, f_values, U=0.1, k0=1, k1=10):
+    """
+    Devuelve el error de una función sobre la función objetivo
+    :param gen_values: Valores a ajustar
+    :param f_values: Valores objetivo
+    :param U: Parámetro de la penalización
+    :param k0: Parámetro de la penalización
+    :param k1: Parámetro de la penalización
+    :return:
+    """
     t1 = sum(abs(f_values - gen_values)[abs(f_values - gen_values) <= U] * k0)
     t2 = sum(abs(f_values - gen_values)[abs(f_values - gen_values) > U] * k1)
     return (t1 + t2) / (len(f_values) + 1)
@@ -153,11 +254,23 @@ def error(gen_values, f_values, U=0.1, k0=1, k1=10):
 
 # Cruzamos
 def one_poin_cross(padre1, padre2):
+    """
+    Método de entrcruzacmiento One Point Cross
+    :param padre1:
+    :param padre2:
+    :return:
+    """
     indice = random.randint(0, len(padre1))
     return padre1[:indice] + padre2[indice:], padre2[:indice] + padre1[indice:]
 
 
 def two_poin_cross(padre1, padre2):
+    """
+    Método de entrcruzacmiento Two Point Cross
+    :param padre1:
+    :param padre2:
+    :return:
+    """
     indice = random.randint(0, len(padre1))
     indice2 = random.randint(0, len(padre1))
     if indice < indice2:
@@ -192,6 +305,13 @@ def swap_mutation(individuo, swap_prob=.5):
 
 
 def torneo(df_gen, poblacion, rand_index):
+    """
+    Selección parental mediante torneo.
+    :param df_gen: DataFrame con la información del fitness de la población
+    :param poblacion: Array con los genotipos de la población
+    :param rand_index: índice aleatorio para seleccionar a los padres
+    :return:
+    """
     if df_gen.iloc[rand_index[0], -1] < df_gen.iloc[rand_index[1], -1]:
         return poblacion[rand_index[0]]
     else:
@@ -199,6 +319,15 @@ def torneo(df_gen, poblacion, rand_index):
 
 
 def evalua(poblacion, dx, N, A, penalty=10e9):
+    """
+    Función que evalua la población
+    :param poblacion: Lista con los genotipos que forman la población
+    :param dx: Variación para la evluación
+    :param N: Número de subintervalos
+    :param A: Límite menor
+    :param penalty: Penalización para individuos incompatibles
+    :return:
+    """
     todo = []
     for p in poblacion:
         p1 = []
@@ -238,6 +367,13 @@ def ret_op_write(codon, x, cod_index=0):
             return var
         except:
             raise ValueError(' Cadena no compatible ')
+    """
+    Función que implementa la decodificación de cadena de números a función matemática en forma analítica
+    :param codon: Cadena de números del 0 al 3
+    :param x: No usado
+    :param cod_index: Código del índice que se quiere tomar para empezar a decodificar la cadena
+    :return: 
+    """
 
     expr_write = ([expr_0_write, 3], [expr_1_write, 3], [expr_2_write, 2], [expr_3_write, 1])
     dic_pre_op_write = ('np.sin', 'np.cos', 'np.exp', 'np.log')
@@ -284,6 +420,16 @@ def ret_op_write(codon, x, cod_index=0):
 
 
 def genera_poblacion(N_POB, min=0, max=4, longitud=60, contador_max=100000, diversidad_inicial = 5):
+    """
+    Función que genera la población teniendo en cuenta la diversidad de genotipos
+    :param N_POB: Número de individuos en la población
+    :param min: Mínimo de la cadena
+    :param max: Máximo de la cadena
+    :param longitud: Longitud de la cadena
+    :param contador_max: Número máximo a partir del cual dejar de asegurar la diversidad
+    :param diversidad_inicial: Parámetro que regula la diversidad inicial.
+    :return:
+    """
     poblacion = []
     expresiones = []
     exc = 0
@@ -325,6 +471,7 @@ def busc_local(df_gen, poblacion, dx, N, A, f_values, N_best = 10):
     df_gen_2 = df_gen_2.sort_values('fitness')
     return list(poblacion), df_gen_2
 
+# Conjunto de funciones del enunciado
 def f_en1(x):
     return x**3 + 8
 def df_en1(x):
